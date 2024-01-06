cb6d4e2ecbeb7eeee2a29a3ec8d6488b6071a2a4
Shaurya Jha <shauryajha1602@gmail.com>
2024-01-06 22:23:45 +0530

    completed methods - GET, POST, DELETE, PATCH

--- server.js
+++ server.js
@@ -1,26 +1,53 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = 8000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "heyy expresser",
+  });
+});
+
+app.get('/:friend', (req, res) => {
+    const {friend} = req.params;
+    const {limit} = req.query;
+
     res.status(200).send({
-        message:'heyy expresser'
+        message: `You asked for friend named ${friend}`
     })
 })
 
-app.post('/', (req, res) => {
-    const {friend} = req.body;
+app.post("/", (req, res) => {
+  const { friend } = req.body;
 
-    if(!friend){
+  if (!friend) {
+    return res.status(400).send({
+      message: "friend not found",
+    });
+
+    res.status(200).send({
+      message: `your friend is called ${friend}`,
+    });
+  }
+});
+
+app.delete('/:friend', (req, res) => {
+    const {friend} = req.params;
+
+    if(friend){
+        res.status(200).send({
+            message: `${friend} is deleted`
+        })
+    }else{
         res.status(400).send({
-            message: 'friend not found'
+            message: 'friend not found in the database'
         })
     }
 })
 
 app.listen(port, () => {
-    console.log(`Server started: ${port}`);
-})
+  console.log(`Server started: ${port}`);
+});
